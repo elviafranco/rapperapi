be7eedd450bd60e53f7714956f7e762fa707c9cb
Lil E <evf@Es-Macbook.local>
2022-06-02 17:33:29 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+const rappers =  {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennet',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+}
+
+
+app.get('/', (req, res) => {
+//   res.send('Hello World!')
+  res.sendFile(__dirname + '/index.html')
+})
+
+//The colon :rapperName lets express know we're working with a query parameter
+app.get('/api/:rapperName', (req, res) => {
+    // .toLowerCase handles casing
+    const rappersName = req.params.rapperName.toLowerCase()
+    // dot notation doesn't play well with spaces so we use bracket notation here
+    if (rappers[rappersName]){
+        res.json(rappers[rappersName])
+    } else {
+        res.json(rappers['dylan'])
+    }
+    })
+
+app.listen(process.env.port || port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
